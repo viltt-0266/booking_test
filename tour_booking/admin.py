5d9c65313b60590bedc326a7fd6168c2b34a372a
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-07-24 15:10:39 +0700

    s

--- tour_booking/admin.py
+++ tour_booking/admin.py
@@ -1,22 +1,42 @@
+# admin.py
 from django.contrib import admin
-from .models import Rating, Booking, Tour, Image
-# Register your models here.
+from .models import Tour, Booking, Image, Rating
 
 class ImageInline(admin.TabularInline):
     model = Image
 
+class TourAmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'date')
+    search_fields = ('name',)
+    list_filter = ('date',)
+    inlines = [ImageInline]
+
+admin.site.register(Tour, TourAmin)
+
+class BookingAmin(admin.ModelAdmin):
+    list_display = ('user', 'tour', 'status', 'created_at', 'departure_date')
+    list_filter = ('status', 'created_at', 'departure_date')
+    actions = ['approve_booking', 'reject_booking']
+
+    def approve_booking(self, request, queryset):
+        queryset.update(status='Approved')
+        # Gửi email cho user khi đơn đặt tour được chấp nhận
+
+    def reject_booking(self, request, queryset):
+        queryset.update(status='Rejected')
+        # Gửi email cho user khi đơn đặt tour bị từ chối
+
+    approve_booking.short_description = "Phê duyệt đơn đặt Tour"
+    reject_booking.short_description = "Từ chối đơn đặt Tour"
+
+admin.site.register(Booking, BookingAmin)
+
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ("user", "tour", "rating", "content", "create_time")
-    fields = [("user"), ("tour"), ("rating"), ("content")]
+    list_display = (
+        "rating",
+    )
 
-@admin.register(Booking)
-class BookingAmin(admin.ModelAdmin):
-    list_display = ( "user", "tour", "status", "created_at","price","number_of_people","departure_date")
-    fields = [ ("user"), ("tour"), ("status")]
-
-@admin.register(Tour)
-class TourAmin(admin.ModelAdmin):
-    list_display = ( "name", "description", "price", "date",)
-    fields = [ ("name"), ("description"), ("price"),("date"),]
-    
+    def has_add_permission(self, request):
+        # Vô hiệu hóa khả năng thêm mới người dùng trong trang quản trị
+        return False
